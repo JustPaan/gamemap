22409b95b5ce037bb184a36e93a237b177262f87
JustPaan <mfarhan1002@gmail.com>
2025-07-21 22:05:38 +0800

    Fix asset loading and add production styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>GameMap</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
